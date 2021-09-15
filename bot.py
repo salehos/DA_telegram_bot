f5a6ab3bfbe0cb3fada4b274cd280f489bc04748
salehos <salehos763@gmail.com>
2021-09-15 22:49:09 +0430

    changing  some config

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import telebot
 import time
 import random
 
-bot = telebot.TeleBot("1668849232:AAGgE5yCtFP2PwAHa9Y7MqcIJzmrJYHuwIQ")
+bot = telebot.TeleBot("token_bot")
+admin_id = "213815860"
 
 def choose_random(number):
     my_list = open("students_list.txt", "r")
@@ -22,7 +23,7 @@ def choose_random(number):
 
 @bot.message_handler(commands=['reset'])
 def reset(message):
-    if str(message.from_user.id) == "210500717":
+    if str(message.from_user.id) == "210500717" or str(message.from_user.id) == admin_id:
         my_list = open("backup.txt", "r")
         students_list = my_list.readlines()
         my_list.close()
@@ -30,59 +31,57 @@ def reset(message):
         for line in students_list:
             new_file.write(line)
         new_file.close()
-        bot.reply_to(message, 'لیست ریست شد.')
+        bot.reply_to(message, 'ﻝیﺲﺗ ﺭیﺲﺗ ﺵﺩ.')
     else:
-        bot.reply_to(message,"شما قادر به انجام این دستور نیستید.")
+        bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
 
-
-@bot.message_handler(commands=['get1'])
+ @bot.message_handler(commands=['get1'])
 def get_a_person(message):
-    if str(message.from_user.id) == "210500717":
+    if str(message.from_user.id) == "210500717" or str(message.from_user.id) ==admin_id:
         a = choose_random(1)
         for student in a:
             bot.send_message(chat_id = "210500717",text=f"{student}")
-            bot.send_message(chat_id = "-599363155",text=f"{student}")
-            # bot.send_message(chat_id = "210500717",text=f"{studet}")
-        else:
-            bot.reply_to(message,"شما قادر به انجام این دستور نیستید.")
+            bot.send_message(chat_id = admin_id,text=f"{student}")
+    else:
+        bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
 
 
 @bot.message_handler(commands=['get2'])
 def get_two_person(message):
-    if str(message.from_user.id) == "210500717":
+    if str(message.from_user.id) == "210500717"or str(message.from_user.id) == admin_id:
         a = choose_random(2)
         for student in a:
             bot.send_message(chat_id = "210500717",text=f"{student}")
-            bot.send_message(chat_id = "-599363155",text=f"{student}")
-            # bot.send_message(chat_id = "210500717",text=f"{studet}")
+            bot.send_message(chat_id = admin_id,text=f"{student}")
     else:
-        bot.reply_to(message,"شما قادر به انجام این دستور نیستید.")
+        bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
 
 @bot.message_handler(commands=['add_to_list'])
 def add_to_list(message):
-    if str(message.from_user.id) == "210500717":
-        msg = bot.reply_to(message, 'نام فردی که میخواهید به لیست اضافه شود رو اعلام کنید.', reply_markup=keyboard)
+    if str(message.from_user.id) == "210500717" or str(message.from_user.id) == admin_id:
+        msg = bot.reply_to(message, 'ﻥﺎﻣ ﻑﺭﺩی کﻩ ﻡیﺥﻭﺎﻫیﺩ ﺐﻫ ﻝیﺲﺗ ﺎﺿﺎﻔﻫ ﺵﻭﺩ ﺭﻭ ﺎﻋﻼﻣ کﻥیﺩ.')
         bot.register_next_step_handler(msg, choosing_one)
     else:
-        bot.reply_to(message,"شما قادر به انجام این دستور نیستید.")
+        bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
+
 
 def choosing_one(message):
     if message.text != None:
         my_list = open("students_list.txt", "r")
         students_list = my_list.readlines()
         my_list.close()
-        students_list.append("\n"+message.text)
+        students_list.append("\n"+message.text)                                                                                                                    
         new_file = open("students_list.txt", "w+")
         for line in students_list:
             new_file.write(line)
-        new_file.close()
-        bot.reply_to(message , "فرد مورد نظر به لیست افراد اضافه شد.")
-
-message_handler(commands=['start'])
+      
+@bot.message_handler(commands=['start'])
 def say_hello(message):
-    bot.reply_to(message,"Welcome")
+    bot.send_message(chat_id = "210500717",text=f"salam + {str(message.from_user.id)}")
+    bot.send_message(chat_id = admin_id, text=f"salam + {str(message.from_user.id)}")
+
     # if str(message.from_user.id) == "210500717":
-    # bot.reply_to(message, 'به بات خوش آمدید. این بات هر جمعه صبح به شما اسامی دو نفر را خواهد داد.')
+    # bot.reply_to(message, 'ﺐﻫ ﺏﺎﺗ ﺥﻮﺷ ﺂﻣﺩیﺩ. ﺍیﻥ ﺏﺎﺗ ﻩﺭ ﺞﻤﻌﻫ ﺺﺒﺣ ﺐﻫ ﺶﻣﺍ ﺎﺳﺎﻣی ﺩﻭ ﻦﻓﺭ ﺭﺍ ﺥﻭﺎﻫﺩ ﺩﺍﺩ.')
     # students_list = backup.split("\n")
     # for i in range(0,42):
     #     a = []
