6f49171d3740352ce829fd4ee42fe4f938e7e657
salehos <salehos763@gmail.com>
2022-01-27 19:30:13 +0330

    Ready to publish

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import telebot
 import time
 import random
+from decouple import config
+ADMIN_ID = config('ADMIN_ID')
+BOT_CREATOR_ID = config('BOT_CREATOR_ID')
+BOT_TOKEN = config('BOT_TOKEN')
 
-bot = telebot.TeleBot("token_bot")
-admin_id = "213815860"
+bot = telebot.TeleBot(BOT_TOKEN)
 
 def choose_random(number):
     my_list = open("students_list.txt", "r")
@@ -23,7 +26,7 @@ def choose_random(number):
 
 @bot.message_handler(commands=['reset'])
 def reset(message):
-    if str(message.from_user.id) == "210500717" or str(message.from_user.id) == admin_id:
+    if str(message.from_user.id) == BOT_CREATOR_ID or str(message.from_user.id) == ADMIN_ID:
         my_list = open("backup.txt", "r")
         students_list = my_list.readlines()
         my_list.close()
@@ -35,30 +38,30 @@ def reset(message):
     else:
         bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
 
- @bot.message_handler(commands=['get1'])
+@bot.message_handler(commands=['get1'])
 def get_a_person(message):
-    if str(message.from_user.id) == "210500717" or str(message.from_user.id) ==admin_id:
+    if str(message.from_user.id) == BOT_CREATOR_ID or str(message.from_user.id) ==ADMIN_ID:
         a = choose_random(1)
         for student in a:
-            bot.send_message(chat_id = "210500717",text=f"{student}")
-            bot.send_message(chat_id = admin_id,text=f"{student}")
+            bot.send_message(chat_id = BOT_CREATOR_ID,text=f"{student}")
+            bot.send_message(chat_id = ADMIN_ID,text=f"{student}")
     else:
         bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
 
 
 @bot.message_handler(commands=['get2'])
 def get_two_person(message):
-    if str(message.from_user.id) == "210500717"or str(message.from_user.id) == admin_id:
+    if str(message.from_user.id) == BOT_CREATOR_ID or str(message.from_user.id) == ADMIN_ID:
         a = choose_random(2)
         for student in a:
-            bot.send_message(chat_id = "210500717",text=f"{student}")
-            bot.send_message(chat_id = admin_id,text=f"{student}")
+            bot.send_message(chat_id = BOT_CREATOR_ID,text=f"{student}")
+            bot.send_message(chat_id = ADMIN_ID,text=f"{student}")
     else:
         bot.reply_to(message,"ﺶﻣﺍ ﻕﺍﺩﺭ ﺐﻫ ﺎﻨﺟﺎﻣ ﺍیﻥ ﺪﺴﺗﻭﺭ ﻥیﺲﺗیﺩ.")
 
 @bot.message_handler(commands=['add_to_list'])
 def add_to_list(message):
-    if str(message.from_user.id) == "210500717" or str(message.from_user.id) == admin_id:
+    if str(message.from_user.id) == BOT_CREATOR_ID or str(message.from_user.id) == ADMIN_ID:
         msg = bot.reply_to(message, 'ﻥﺎﻣ ﻑﺭﺩی کﻩ ﻡیﺥﻭﺎﻫیﺩ ﺐﻫ ﻝیﺲﺗ ﺎﺿﺎﻔﻫ ﺵﻭﺩ ﺭﻭ ﺎﻋﻼﻣ کﻥیﺩ.')
         bot.register_next_step_handler(msg, choosing_one)
     else:
@@ -77,22 +80,9 @@ def choosing_one(message):
       
 @bot.message_handler(commands=['start'])
 def say_hello(message):
-    bot.send_message(chat_id = "210500717",text=f"salam + {str(message.from_user.id)}")
-    bot.send_message(chat_id = admin_id, text=f"salam + {str(message.from_user.id)}")
+    bot.send_message(chat_id = BOT_CREATOR_ID,text=f"salam + {str(message.from_user.id)}")
+    bot.send_message(chat_id = ADMIN_ID, text=f"salam + {str(message.from_user.id)}")
 
-    # if str(message.from_user.id) == "210500717":
-    # bot.reply_to(message, 'ﺐﻫ ﺏﺎﺗ ﺥﻮﺷ ﺂﻣﺩیﺩ. ﺍیﻥ ﺏﺎﺗ ﻩﺭ ﺞﻤﻌﻫ ﺺﺒﺣ ﺐﻫ ﺶﻣﺍ ﺎﺳﺎﻣی ﺩﻭ ﻦﻓﺭ ﺭﺍ ﺥﻭﺎﻫﺩ ﺩﺍﺩ.')
-    # students_list = backup.split("\n")
-    # for i in range(0,42):
-    #     a = []
-    #     for i in range(0,2):
-    #         choosen_one = random.randint(0,len(students_list)-1)
-    #         bot.send_message(chat_id= message.chat.id , text= students_list[choosen_one])
-    #         a.append(students_list[choosen_one])
-    #         del students_list[choosen_one]
-    #     # for save a list from who choosed before
-    #     done_list = open("student_list.txt", "a+")
-    #     done_list.write(a)
 
 while True:
     try:
